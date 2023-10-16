3ad0f018055a30e4eb6edd1be9f6ada31429026b
herve perel <herveperel@gmail.com>
2023-10-16 10:04:40 +0200

    express4

--- movieHandlers.js
+++ movieHandlers.js
@@ -101,18 +101,68 @@ const postMovie = (req, res) => {
 const postUser = (req, res) => {
   const { firstname, lastname, email, city, language } = req.body;
 
-  database.query(
-    "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
-    [firstname, lastname, email, city, language]
-  )
-  .then(([result]) => {
-    res.location(`/api/users/${result.insertId}`);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error saving the user")
-  })
-}
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+  database
+    .query(
+      "update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?",
+
+      [title, director, year, color, duration, id]
+    )
+
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+
+    .catch((err) => {
+      console.error(err);
+
+      res.status(500).send("Error editing the movie");
+    });
+};
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ?",
+      [firstname, lastname, email, city, language]
+    )
+
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+
+    .catch((err) => {
+      console.error(err);
+
+      res.status(500).send("Error editing the user");
+    });
+};
 
 module.exports = {
   getMovies,
@@ -121,5 +171,7 @@ module.exports = {
   getUsers,
   getUserById,
   postMovie,
-  postUser
+  postUser,
+  updateMovie,
+  updateUser,
 };
